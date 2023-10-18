f4d66668561781a7234238947312b6558b582f98
silalm <silviamoura_17@hotmail.com>
2023-10-18 09:37:07 +0100

    gg

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,9 +60,7 @@ export default function Home() {
             <h2>
               Docs <span>-&gt;</span>
             </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
+            
           </a>
 
           <a
