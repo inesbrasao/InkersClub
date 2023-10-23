422bce7cdc2aaa3af2d4fd168476ca83fd3b7cba
e-paiva <evellynpaiva01@gmail.com>
2023-10-23 17:35:13 +0100

    edit componentes signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,65 +1,62 @@
 import InputText from "@/app/componentes/InputText";
-import { Router } from "next/router";
+import { Router, useRouter } from "next/router";
 
 import React, { useState } from 'react';
 
 export default function SignUp() {
-    const [formData, setFormData] = useState()
-    const [artistId, setArtistId] = useState()
+  const [formData, setFormData] = useState()
+  const [artistId, setArtistId] = useState()
+  const router = useRouter()
 
-    const options = {
-      method: 'POST',
-      headers: {
-          'Content-Type': "application/json"
-      },
-      body: JSON.stringify({"collection": "artists",
-                          "id": idState})
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify(formData)
   }
 
-    async function signup() {
-        const res = await fetch('/api/signup', options)
+  async function signup() {
+    const res = await fetch('/api/signup', options)
 
-        if (res.status === 200) {
-            const body = await res.json();
-            console.log("listcard", body)
-            
-         }
-        
+    if (res.status === 200) {
+      const body = await res.json();
+      
+      router.push(`/myprofile/form/${body._id}`)
     }
 
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    
-        
-        const { email, password, passwordConfirm } = formData;
-        signup(formData)
-        
-       
-    
-      };
-  
-      const handleChange = (event) => {
-        const { name, value } = event.target;
-        console.log(event.target.value,name)
-        setFormData({
-          ...formData,
-          [name]: value,
-        });
-      };
-
-
-
-    return (
-          <div>
-          <h1>Criar Conta</h1>
-          <form>
-          <InputText name="Email" label="Email" onChange={handleChange}/>
-          <InputText name="password" label="Senha" onChange={handleChange}/>
-          <InputText name="passwordConfirm" label="Confirmar Senha" onChange={handleChange}/>
-
-          <button type="submit" onSubmit={handleSubmit}>Continuar</button>
-          </form>
-          </div>
-    )
   }
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    signup(formData)
+    console.log(formData)
+
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(event.target.value, name)
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+
+
+  return (
+    <div>
+      <h1>Criar Conta</h1>
+      <form onSubmit={handleSubmit}>
+        <InputText name="email" label="Email" onChange={handleChange} />
+        <InputText name="password" label="Senha" onChange={handleChange} />
+        <InputText name="passwordConfirmation" label="Confirmar Senha" onChange={handleChange} />
+
+        <button type="submit" >Continuar</button>
+      </form>
+    </div>
+  )
+}
