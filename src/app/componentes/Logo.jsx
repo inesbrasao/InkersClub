890eff6b19e84a7cf4d73d9c0c0adcc7645da0f3
silalm <silviamoura_17@hotmail.com>
2023-10-24 11:02:00 +0100

    CSS & logo

--- src/app/componentes/Logo.jsx
+++ src/app/componentes/Logo.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import styles from '@/styles/index.module.css';
 
 class Logo extends Component {
   render() {
     return (
-      <div className="logo-test">
-         <img src="./src/images/Inkers_club.png" alt="test logo"/>
+      <div>
+        <img src= "\background\Inkers__club.png" width={320} height={320} className={styles.logo}/>
       </div>
     )
   }
