af2f5d819bf29e24c9763766f605c6276451c613
ines.balt <ines.brasao@gmail.com>
2023-10-27 09:09:04 +0100

    nav bar finished

--- src/app/componentes/NavBar.jsx
+++ src/app/componentes/NavBar.jsx
@@ -10,9 +10,9 @@ export default function NavBar() {
 
    return <>
    <div className={styles.nav}>
-   <button className={styles.button}><img className={styles.icon} src="/icons/search-button.svg" alt="" /></button>
-   <button className={styles.button}><img className={styles.icon} src="/icons/home-button.svg" alt="" /></button>
-   <button className={styles.button}><img className={styles.icon} src="/icons/return-button.svg" alt="" /></button>
+   <button className={styles.button} onClick={() => router.push('/home')}><img className={styles.icon} src="/icons/search-button.svg" alt="" /></button>
+   <button className={styles.button} onClick={() => router.push('/')}><img className={styles.icon} src="/icons/home-button.svg" alt="" /></button>
+   <button className={styles.button} onClick={() => router.back()}><img className={styles.icon} src="/icons/return-button.svg" alt="" /></button>
    </div>
    </>
 }
