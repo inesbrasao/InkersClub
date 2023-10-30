4230b19b3223822a1e8501ca78bdeb2ef47f80ad
ines.balt <ines.brasao@gmail.com>
2023-10-30 14:11:46 +0000

    b

--- src/app/componentes/CardImage.jsx
+++ src/app/componentes/CardImage.jsx
@@ -6,24 +6,30 @@ import styles from '@/styles/cardImage.module.css'
 // }
 // CardImage Component - shows image and correspondent tags
 export default function CardImage({ image, page }) {
+
+    
     const pathImage = image.path
     const tags = image.tag
-    const artistName = image.artist_id   
-
-
-    return <>
+    const artistName = image.artist_id
     
-    <div className={page === "home" ? styles.cardImage : styles.cardImage2}>
-    <div className={styles.image}
+
+
+
+    return (
+    <>
+    
+        <div className={page === "home" ? styles.cardImage : styles.cardImage2}>
+            <div className={styles.image}
        style={{backgroundImage: `url(/api/loadimages/${pathImage.split("/")[2]})`,  backgroundPosition: "center center",  width: "160px", height: "160px"}}
        alt={artistName}
-    />
+            />
     
-    {tags.map(e=><div key={e} className={page === "home" ? styles.tags : styles.tags2}>{e}</div>)}
+            {tags?.map(e=><div key={e} className={page === "home" ? styles.tags : styles.tags2}>{e}</div>)}
    
-    </div>
+        </div>
     </>
 
+    )
 }
 
 
